a6a537e83c26ed238049c3219586b20f1fa75494
IsabellaRaio <bellaraio24@gmail.com>
2026-04-15 19:10:56 -0300

    Delete target directory

--- target/debug/deps/project.d
+++
@@ -1,5 +0,0 @@
-C:\Users\vinia\OneDrive\Área de Trabalho\Projetos Java\testes\Project\target\debug\deps\project.d: src\main.rs
-
-C:\Users\vinia\OneDrive\Área de Trabalho\Projetos Java\testes\Project\target\debug\deps\project.exe: src\main.rs
-
-src\main.rs:
--- target/debug/project.d
+++
@@ -1,1 +0,0 @@
-C:\Users\vinia\OneDrive\Área\ de\ Trabalho\Projetos\ Java\testes\Project\target\debug\project.exe: C:\Users\vinia\OneDrive\Área\ de\ Trabalho\Projetos\ Java\testes\Project\src\main.rs
